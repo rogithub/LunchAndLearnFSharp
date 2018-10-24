a661e774d3a78e4a549e4426c42c995ea406f673
rodrigo debian <correo.rodrigo@gmail.com>
2018-10-24 15:55:55 -0500

    generation

--- program.fsx
+++ program.fsx
@@ -3,11 +3,33 @@
 #load "Encryption.fs"
 #load "Product.fs"
 
+open System
 open Product
 
-let water = { Name="Water"; Percent=10.00 }
-let sugar = { Name="Sugar"; Percent=10.00 }
-let coffeeGrains = { Name="Coffe Grains"; Percent=80.00 }
-let prodCofee = { Name="Coffee"; Formulation=[|water; coffeeGrains; sugar|]; FormulationEncrypted="" }
+let rand = Random()
+let beverages = [|"Wine"; "Beer"; "Yogurth"; "Coffee"; "Soft drink"; "Orange Juice"; "Wiskey"; "Lemonade"|]
+let ingredients = [|"Water"; "Alcohol"; "Flavor"; "Sugar"; "Milk"; "Vitamin A"; "Row Malt"; "Coffe Grains"; "Calcium Carbonate"; "Yeast"; "Vanilla extract"|]
 
-toString prodCofee |> fromString
+let rec getElementFromArray (arr:'a[]) =
+    let newVal = rand.Next(0, arr.Length);
+    arr.[newVal]
+
+let arrayGenerator () =    
+    seq { 0 .. rand.Next(0, 9) .. 20 } |> Seq.toArray
+
+let ingredientsGenerator =
+    arrayGenerator() |> Array.map (fun x -> { Name=getElementFromArray ingredients; Percent=(float x)} )
+
+let productGenerator amount =        
+    [ for i in 0 .. amount -> i] |> List.map (fun x -> { Name=getElementFromArray beverages; Formulation=ingredientsGenerator; FormulationEncrypted="" } )
+    
+
+
+
+
+///let water = { Name="Water"; Percent=10.00 }
+///let sugar = { Name="Sugar"; Percent=10.00 }
+///let coffeeGrains = { Name="Coffe Grains"; Percent=80.00 }
+///let prodCofee = { Name="Coffee"; Formulation=[|water; coffeeGrains; sugar|]; FormulationEncrypted="" }
+
+///toString prodCofee |> fromString
