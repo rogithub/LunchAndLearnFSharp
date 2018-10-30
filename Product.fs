3ed7ac1a40339f1d760914eada328f1981a31061
Rodrigo Debian <rosec@protonmail.ch>
2018-10-29 21:48:18 -0500

    progress

--- Product.fs
+++ Product.fs
@@ -5,14 +5,31 @@ open Encryption
 [<CLIMutable>]
 type FormulaItem = string * float
 
-type Product = { Id: int; Name: string; Formulation: FormulaItem[]; FormulaEncrypted: string }
+type Encrypted = { Id: int; Name: string; Formulation: string }
+type Decrypted = { Id: int; Name: string; Formulation: FormulaItem[] }
 
-let encryptFormulation (formulation: FormulaItem[]) =
-    let encryptedFormula = formulation |> serializeJson |> encrypt
-    encryptedFormula
-    
+type Encryptor<'prod, 'formula> = FEncryptor of 'prod * 'formula
+ 
+let bind = function 
+    | (prod, formula) -> FEncryptor(prod, formula)
 
-let decryptFormulation (formulation: string) =
-    let decryptedFormula = decrypt formulation |> deserialiseJson<FormulaItem[]>
-    decryptedFormula
+let run = function
+    | FEncryptor(prod, formula) -> (prod, formula)
+
+let map fx = function
+    | FEncryptor(prod, formula) -> FEncryptor(fx prod, formula)
+
+let flatMap fx = function
+    | FEncryptor(prod, formula) -> 
+        let (prod, new_formula) = run (fx prod)
+        FEncryptor(prod, Array.append formula new_formula)
+
+
+let encryptProd (prod:Decrypted) =
+    let encryptedFormula = prod.Formulation |> serializeJson |> encrypt
+    { Encrypted.Id = prod.Id; Name = prod.Name; Formulation = encryptedFormula }
+
+let decryptProd (prod:Encrypted) =
+    let decryptedFormula = decrypt prod.Formulation |> deserialiseJson<FormulaItem[]>
+    { Decrypted.Id = prod.Id; Name = prod.Name; Formulation = decryptedFormula } 
 
--- program.fsx
+++ program.fsx
@@ -2,22 +2,12 @@
 #load "Serialization.fs"
 #load "Encryption.fs"
 #load "Product.fs"
-#load "ProductMonad.fs"
-#load "Agents.fs"
 
 open System
 open Product
-open ProductMonad
-open Agents
 
+let cupOfCoffe = encryptProd { Id = 1; Name = "Cup of Coffe"; Formulation = [| ("Water", 80.0); ("Sugar", 10.0); ("Coffee", 10.0) |]; }
+let orangeJuice = encryptProd { Id = 2; Name = "Orange Juice"; Formulation = [| ("Water", 80.0); ("Vitamins", 10.0); ("Orange", 10.0) |]; }
+let laptop = encryptProd { Id = 3; Name = "Laptop"; Formulation = [| ("Plastic", 80.0); ("Hard Disk", 10.0 ); ("RAM", 10.0) |]; }
 
-let cupOfCoffe = { Id = 1; Name = "Cup of Coffe"; Formulation = [| ("Water", 80.0); ("Sugar", 10.0); ("Coffee", 10.0) |]; FormulaEncrypted = "" }
-let orangeJuice = { Id = 2; Name = "Orange Juice"; Formulation = [| ("Water", 80.0); ("Vitamins", 10.0); ("Orange", 10.0) |]; FormulaEncrypted = "" }
-let laptop = { Id = 3; Name = "Laptop"; Formulation = [| ("Plastic", 80.0); ("Hard Disk", 10.0 ); ("RAM", 10.0) |]; FormulaEncrypted = "" }
-
-let encrypted = encryptFormulation <| cupOfCoffe.Formulation
-let decripted1 = decryptFormulation <| encrypted
-
-decripted1 = cupOfCoffe.Formulation
-
-
+bind (cupOfCoffe, cupOfCoffe.Formulation)
