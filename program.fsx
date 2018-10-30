cb35ae48b3b5a95e0dd422530f664790266eb09b
Rodrigo Debian <rosec@protonmail.ch>
2018-10-29 22:21:19 -0500

    fixed typo

--- program.fsx
+++ program.fsx
@@ -6,7 +6,7 @@
 open System
 open Product
 
-let cupOfCoffe = ({ Id = 1; Name = "Cup of Coffe" }, [| ("Water", 80.0); ("Sugar", 10.0); ("Coffee", 10.0) |]) 
+let cupOfCoffe = ({ Id = 1; Name = "Cup of Coffee" }, [| ("Water", 80.0); ("Sugar", 10.0); ("Coffee", 10.0) |]) 
 let orangeJuice = ({ Id = 2; Name = "Orange Juice" }, [| ("Water", 80.0); ("Vitamins", 10.0); ("Orange", 10.0) |])
 let laptop = ({ Id = 3; Name = "Laptop" }, [| ("Plastic", 80.0); ("Hard Disk", 10.0 ); ("RAM", 10.0) |])
 
