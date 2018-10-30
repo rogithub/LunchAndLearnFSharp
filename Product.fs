7c51cc125e669d1253ee1bd862f1f5578416b914
rodrigo debian <correo.rodrigo@gmail.com>
2018-10-30 11:18:11 -0600

    testing monads

--- Product.fs
+++ Product.fs
@@ -7,22 +7,8 @@ type FormulaItem = string * float
 
 type Product = { Id: int; Name: string; }
 
+//https://hackernoon.com/a-monad-writer-for-f-26aa987e4a3a
 type Encryptor<'prod, 'formula> = FEncrypt of 'prod * 'formula
-
-let bind = function 
-    | (prod, formula) -> FEncrypt(prod, formula)
-
-let run = function
-    | FEncrypt(prod, formula) -> (prod, formula)
-
-let map fx = function
-    | FEncrypt(prod, formula) -> FEncrypt(fx prod, formula)
-
-let flatMap fx = function
-    | FEncrypt(prod, formula) -> 
-        let (prod, new_formula) = run (fx prod)
-        FEncrypt(prod, Array.append formula new_formula)
-
 
 let encryptFormula (formula:FormulaItem[]) =
     let encryptedFormula = formula |> serializeJson |> encryptText
@@ -32,4 +18,20 @@ let decryptFormula (formula:string) =
     let decryptedFormula = decryptText formula |> deserialiseJson<FormulaItem[]>
     decryptedFormula
 
-let lift (prod, formula) = bind (prod, encryptFormula(formula))
+let bind = function
+    | (prod, formula) -> FEncrypt(prod, encryptFormula formula)
+
+let run = function
+    | FEncrypt(prod, formula) -> (prod, decryptFormula formula)
+
+let map f = function
+    | FEncrypt(prod, formula) ->
+        let mappedProd = f (prod)
+        FEncrypt(mappedProd, formula)
+
+let flatMap fx = function
+    | FEncrypt(prod, formula) ->
+        let (prod1, decrypted) = run (prod formula)
+        let (prod2, newDecrypted) = fx (prod1, decrypted)
+        let encrypted = encryptFormula newDecrypted
+        FEncrypt(prod2, encrypted)
